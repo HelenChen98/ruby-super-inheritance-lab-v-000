0b20e6b49862074a071789b07bc0645d60191f24
Karen C <chenyanhuan66@163.com>
2018-08-12 07:38:28 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,7 +1,7 @@
 class ChattyStudent < Student
 
   def raise_hand
-    10.times do |i| 
+    10.times do |i|
       super
     end
   end
@@ -10,5 +10,5 @@ class ChattyStudent < Student
     super
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
   end
-  
+
 end
